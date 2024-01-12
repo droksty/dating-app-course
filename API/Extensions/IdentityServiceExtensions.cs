a6892654a7bfbfe1e7288fc9391bfe58302401be
droksty <droksty@gmail.com>
2024-01-12 16:17:15 +0200

    1.1.7 cleanup with extension methods

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Extensions
+{
+	public static class IdentityServiceExtensions
+	{
+		public static IServiceCollection AddIdentityServices(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+				.AddJwtBearer(options => 
+				{
+					options.TokenValidationParameters = new TokenValidationParameters
+					{
+						// Rules about how the service should validate this token is a good token
+						ValidateIssuerSigningKey = true,
+						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]!)),
+						ValidateIssuer = false,
+						ValidateAudience = false
+					};
+				});
+
+			return services;
+		}
+	}
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,38 +1,16 @@
-using API.Data;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-	opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
-builder.Services.AddScoped<ITokenService, TokenServiceImpl>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-	.AddJwtBearer(options =>
-	{
-		options.TokenValidationParameters = new TokenValidationParameters
-		{
-			// Rules about how the service should validate this token is a good token
-			ValidateIssuerSigningKey = true,
-			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"]!)),
-			ValidateIssuer = false,
-			ValidateAudience = false
-		};
-	});
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 
+// Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
  
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
